4efdede6e7cd8e09e7f40b4a6de457c951a24edf
ThilinaGX <thilinajp8@gmail.com>
2026-02-28 11:17:01 +0530

    feature: write to file using fs module

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-const fs = require('fs');
+const myPromise = new Promise((resolve, reject) => {
+  const condition = true; // change to false to see rejection
 
-fs.writeFile('file.txt', 'Hello World!', function (err) {
-  if (err) throw err;
-  console.log('File saved!');
+  if (condition) {
+    resolve('Success!');
+  } else {
+    reject('Failure!');
+  }
 });
+
+async function myFunction() {
+  try {
+    const result = await myPromise;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+myFunction();
